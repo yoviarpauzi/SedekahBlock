3d752465826904395bc6d32f46e0a505f1ffc415
yoviarpauzi <yoviarpauzi99@gmail.com>
2025-04-20 13:43:20 +0700

    create script for createCampaign

---
+++ smartcontract/scripts/createCampaign.ts
@@ -0,0 +1,23 @@
+import { Address, OpenedContract, toNano } from '@ton/core';
+import { Donation } from '../wrappers/Donation';
+import { NetworkProvider } from '@ton/blueprint';
+
+export async function run(provider: NetworkProvider) {
+    const contractAddress: Address = Address.parse('0QBEsW5QAOSjha2T2a6PKpVet2H5ToDg1f06P_VkQZUMnUQ3');
+    const donation: OpenedContract<Donation> = provider.open(Donation.fromAddress(contractAddress));
+
+    await donation.send(
+        provider.sender(),
+        {
+            value: toNano('0.05'),
+        },
+        {
+            $$type: 'CreateCampaign',
+            id: 1n,
+            name: 'campaign 1',
+        },
+    );
+
+    const campaignName = await donation.getCampaignName(1n);
+    console.log(campaignName);
+}
